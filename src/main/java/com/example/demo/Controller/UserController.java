b5c6d29631cf71ce9b1eec623d609680daa2554f
avel <avel40m@gmail.com>
2022-06-28 20:50:25 -0300

    second commit - swagger

--- src/main/java/com/example/demo/Controller/UserController.java
+++ src/main/java/com/example/demo/Controller/UserController.java
@@ -2,6 +2,8 @@ package com.example.demo.Controller;
 
 import com.example.demo.Entity.Users;
 import com.example.demo.Service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
@@ -13,28 +15,47 @@ import java.util.List;
 public class UserController {
     @Autowired
     private UserService userService;
-
+    private static final Logger log = LoggerFactory.getLogger(UserController.class);
     @GetMapping("/")
     public ResponseEntity<List<Users>> listUser(){
         List<Users>  users = userService.listUser();
+        log.warn("List of users");
         return ResponseEntity.ok(users);
     }
 
     @PostMapping("/create")
     public ResponseEntity<Users> createUser(@RequestBody Users users){
-        Users u = userService.createUser(users);
-        return ResponseEntity.ok(u);
+        try {
+            Users u = userService.createUser(users);
+            log.warn("Create a user with id " + u.getId());
+            return ResponseEntity.ok(u);
+        } catch (Exception e) {
+            log.error("Error: " + e.getMessage());
+            return ResponseEntity.badRequest().build();
+        }
     }
 
     @PutMapping("/edit")
     public ResponseEntity<Users> editUser(@RequestBody Users users){
-        Users u = userService.editUser(users);
-        return ResponseEntity.ok(u);
+        try {
+            Users u = userService.editUser(users);
+            log.info("Edit user the id " + users.getId());
+            return ResponseEntity.ok(u);
+        } catch (Exception e){
+            log.error("Error: " + e.getMessage());
+            return ResponseEntity.badRequest().build();
+        }
     }
 
     @DeleteMapping("/delete/{id}")
     public ResponseEntity<String> deleteUser(@PathVariable String id){
-        userService.deleteUser(id);
-        return ResponseEntity.ok("Deleted correctly");
+        try {
+            userService.deleteUser(id);
+            log.warn("the id is removed " + id);
+            return ResponseEntity.ok("Deleted correctly");
+        } catch (Exception e){
+            log.error("Error " + e.getMessage());
+            return ResponseEntity.badRequest().build();
+        }
     }
 }
