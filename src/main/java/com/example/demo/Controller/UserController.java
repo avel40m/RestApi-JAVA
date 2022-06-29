b5e50bc0ef382ac87db4d0fad36839fc266892f0
avel <avel40m@gmail.com>
2022-06-29 13:08:47 -0300

    tree commit - controller

--- src/main/java/com/example/demo/Controller/UserController.java
+++ src/main/java/com/example/demo/Controller/UserController.java
@@ -6,6 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -31,7 +32,8 @@ public class UserController {
             return ResponseEntity.ok(u);
         } catch (Exception e) {
             log.error("Error: " + e.getMessage());
-            return ResponseEntity.badRequest().build();
+
+            return new ResponseEntity(e.getMessage(), HttpStatus.NOT_FOUND);
         }
     }
 
---
+++ src/test/java/com/example/demo/PasswordTest.java
@@ -0,0 +1,7 @@
+package com.example.demo;
+
+public class PasswordTest {
+    public static void main(String[] args) {
+
+    }
+}
