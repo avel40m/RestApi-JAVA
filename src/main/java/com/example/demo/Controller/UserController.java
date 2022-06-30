73a70649fee88502fb270096a58364fe2317c490
avel <avel40m@gmail.com>
2022-06-30 18:20:30 -0300

    four commit - jwt

--- src/main/java/com/example/demo/Controller/UserController.java
+++ src/main/java/com/example/demo/Controller/UserController.java
@@ -1,13 +1,20 @@
 package com.example.demo.Controller;
 
+import antlr.ASdebug.TokenOffsetInfo;
+import com.example.demo.Config.JwtUtilService;
+import com.example.demo.Config.TokenInfo;
+import com.example.demo.Config.UsuarioDetailsService;
+import com.example.demo.Config.AuthenticationReq;
 import com.example.demo.Entity.Users;
 import com.example.demo.Service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,6 +23,12 @@ import java.util.List;
 public class UserController {
     @Autowired
     private UserService userService;
+    @Autowired
+    private AuthenticationManager authenticationManager;
+    @Autowired
+    private UsuarioDetailsService usuarioDetailsService;
+    @Autowired
+    private JwtUtilService jwtUtilService;
     private static final Logger log = LoggerFactory.getLogger(UserController.class);
     @GetMapping("/")
     public ResponseEntity<List<Users>> listUser(){
@@ -60,4 +73,21 @@ public class UserController {
             return ResponseEntity.badRequest().build();
         }
     }
+
+    @PostMapping("/public/authenticate")
+    public ResponseEntity<TokenInfo> authenticate(@RequestBody AuthenticationReq authenticationReq){
+        log.info("Authenticate user {}",authenticationReq.getUsuario());
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(authenticationReq.getUsuario(),authenticationReq.getClave()));
+
+        final UserDetails userDetails = usuarioDetailsService.loadUserByUsername(authenticationReq.getUsuario());
+
+        final String jwt = jwtUtilService.generateToken(userDetails);
+
+        TokenInfo tokenInfo = new TokenInfo(jwt);
+
+        return ResponseEntity.ok(tokenInfo);
+
+    }
+
 }
